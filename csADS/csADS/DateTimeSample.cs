bf61faa0834e2389b437eae651f82758ada682da
Jack <hqc821@163.com>
2019-07-30 06:47:15 +0800

    still need more coding to finalize the date,time read/Write

--- csADS/csADS/DateTimeSample.cs
+++ csADS/csADS/DateTimeSample.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,6 +26,8 @@ namespace csADS
 
         private void DateTime_Load(object sender, EventArgs e)
         {
+            CheckForIllegalCrossThreadCalls = false;
+
             client = new TcAdsClient();
             client.Connect(851);
             try
@@ -33,21 +36,26 @@ namespace csADS
                 hdate1 =  client.CreateVariableHandle("MAIN.date1");
 
                 client.AddDeviceNotificationEx("MAIN.time1", AdsTransMode.OnChange, 100, 0, txtTime, typeof(TimeSpan));
-
+                //client.AddDeviceNotificationEx("MAIN.date1", AdsTransMode.OnChange, 100, 0, txtDate, typeof(DateTime));
                 client.AdsNotificationEx += AdsNotificationEx;
 
             }
-            catch (Exception)
+            catch (Exception err)
             {
-                throw;
+                MessageBox.Show("DateTime Form load " + err.Message);
+                //throw;
             }
-        }
+        }        
 
         private void AdsNotificationEx(object sender, AdsNotificationExEventArgs e)
         {
             if (e.Value.GetType() == typeof(TimeSpan))
             {
-                txtTime.Text = e.Value.ToString();
+                txtTime.Text = ((TimeSpan)e.Value).ToString();
+            }
+            else if (e.Value.GetType() == typeof(DateTime))
+            {
+                txtDate.Text = ((DateTime)e.Value).ToString();
             }
         }
     }
